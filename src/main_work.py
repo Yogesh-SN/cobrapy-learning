afef063d09a8cdba5b4f937b75f54a3d0e2d0291
Yogesh Singh Negi <yogeshnegi987@gmail.com>
2026-06-18 11:41:46 +0530

    Imported flux sampling functions onto main_work src

--- src/main_work.py
+++ src/main_work.py
@@ -1,15 +1,15 @@
-from helpers import load_model
-from helpers import biomass_reaction
+from helpers import load_model, biomass_reaction, flux_sampling, flux_sampling_to_csv
 
 def main():
-   model_path = "/home/yogi/programming/cobra_learning/models/e_coli_core.xml"  
+   model_path = "/home/yogi/programming/cobrapy-learning/models/e_coli_core.xml"  
    model = load_model(model_path)
 
-
    print("Loaded in main.py")
-   print(model.summary())
+   #print(model)
 
    print(biomass_reaction(model))
+
+   flux_sampling_to_csv(model, 100, 'test_sampling')
 
 if __name__ == "__main__":
    main()
